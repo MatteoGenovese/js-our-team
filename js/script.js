9368af9246ce0b98cd291b5513b10a287c2234a5
MatteoGenovese <matteo.genovese@icloud.com>
2022-06-28 15:44:40 +0200

    Bonus 3

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
 // MILESTONE 1:
 // Stampare su console le informazioni di nome, ruolo e la stringa della foto
 
-team = [{
+const team = [{
         name: "Wayne Barnett",
         role: "Founder & CEO",
         photo: "wayne-barnett-founder-ceo.jpg"
@@ -35,29 +35,31 @@ team = [{
         name: "Barbara Ramos",
         role: "Graphic Designer",
         photo: "barbara-ramos-graphic-designer.jpg"
-    },
-]
+    }
+];
 
 // for (let employee in team) {
 //     printOnConsole(team[employee].name, team[employee].role, team[employee].photo)
 // }
 
-function printOnConsole(nome, ruolo, foto) {
-    console.log(`
-    Nome:   ${nome}
-    Ruolo:  ${ruolo}
-    Foto:   ${foto}
-    
-    `)
+
+
+function printOnConsole() {
+    for (let employee in team) {
+        console.log(`
+        Nome:   ${team[employee].name}
+        Ruolo:  ${team[employee].role}
+        Foto:   ${team[employee].photo}
+        
+        `)
+    }
 }
 // MILESTONE 2:
 // Stampare le stesse informazioni su DOM sottoforma di stringhe
 
-const teamElement = document.getElementById("team");
-
-for (let employee in team) {
-    printOnDom(team[employee].name, team[employee].role, team[employee].photo)
-}
+let teamElement = document.getElementById("team");
+printOnDom();
+printOnConsole();
 
 //Bonus 1 Trasformare la stringa foto in una immagine effettiva
 
@@ -71,16 +73,82 @@ for (let employee in team) {
 
 //Bonus 2 Organizzare i singoli membri in card/schede
 
-function printOnDom(nome, ruolo, foto) {
-    teamElement.innerHTML += `
-    <div class="col-4 mt-3 d-flex justify-content-center">
-        <div class="card" style="width: 18rem;">
-            <img src="./img/${foto}" class="card-img-top" alt="foto di ${nome} ">
-            <div class="card-body">
-                <h5 class="card-title">${nome}</h5>
-                <p class="card-text">${ruolo}.</p>
-            </div> 
-        </div>
-    </div>
-    `;
+
+
+// let str = 'JavaScript String Startswith';
+
+// console.log(str.startsWith('Java'));
+
+
+function printOnDom() {
+    //svuoto le informazioni che ci sono prima di ogni nuova stampa
+    teamElement.innerHTML = "";
+    for (employee in team) {
+        if ((team[employee].photo).includes('http')) {
+
+            console.log("sono nell'if");
+
+            teamElement.innerHTML += `
+                <div class="col-4 mt-3 d-flex justify-content-center">
+                    <div class="card" style="width: 18rem;">
+                        <img src="${team[employee].photo}" class="card-img-top" alt="foto di ${team[employee].name} ">
+                        <div class="card-body">
+                            <h5 class="card-title">${team[employee].name}</h5>
+                            <p class="card-text">${team[employee].role}</p>
+                        </div> 
+                    </div>
+                </div>
+                `;
+        } else {
+
+            console.log("sono nell'else");
+            console.log(teamElement.innerHTML);
+
+            teamElement.innerHTML += `
+            <div class="col-4 mt-3 d-flex justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <img src="./img/${team[employee].photo}" class="card-img-top" alt="foto di ${team[employee].name} ">
+                    <div class="card-body">
+                        <h5 class="card-title">${team[employee].name}</h5>
+                        <p class="card-text">${team[employee].role}</p>
+                    </div> 
+                </div>
+            </div>
+            `;
+        }
+    }
+}
+
+//Bonus 3 Inserisci un nuovo impiegato tramite form
+
+const insertANewEmployeeButton = document.getElementById("insertEmployee");
+
+insertANewEmployeeButton.addEventListener('click', function() {
+    //estrai i dati
+    console.log(insertANewEmployeeButton);
+    const newName = document.getElementById("newName");
+    const newRole = document.getElementById("newRole");
+    const newUrl = document.getElementById("newUrl");
+
+    //inserisci nell'array
+
+    insertInDatabase(newName.value, newRole.value, newUrl.value);
+
+    //stampali
+    printOnDom();
+
+    printOnConsole();
+})
+
+
+function insertInDatabase(nome, ruolo, url) {
+
+    newItem = {
+        name: nome,
+        role: ruolo,
+        photo: url
+    }
+
+    team.push(newItem);
+
 }
